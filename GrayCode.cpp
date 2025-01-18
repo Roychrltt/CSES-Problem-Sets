a98b4d3ac076fd21e0fe70b71f81ad24671402ee
Charlotte <roychrltt@gmail.com>
2025-01-18 14:06:27 +0100

    Go to school

--- GrayCode.cpp
+++ GrayCode.cpp
@@ -7,10 +7,21 @@ int main()
 	int n;
 	std::cin >> n;
 	int total = 1 << n;
-    for (int i = 0; i < total; i++)
+	for (int i = 0; i < total; i++)
 	{
-        int gray = i ^ (i >> 1); 
+		int gray = i ^ (i >> 1); 
 		std::cout << std::bitset<16>(gray).to_string().substr(16 - n) << std::endl; 
-    }
+	}
 	return 0;
 }
+
+/*  n = 1  n = 2  n = 3  n = 4
+ *	  0		 0		00	  000	
+ *			 1		01	  001
+ *					11    011
+ *					10	  010
+ *						  110
+ *						  111
+ *						  101
+ *						  100
+ */
