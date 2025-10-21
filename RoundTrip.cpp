c323e15b30a37b006dcd1e33e218e67d830f0be6
Xiaoyun Xu <xiaxu@f4r7s12.clusters.42paris.fr>
2025-10-21 17:54:50 +0200

    .

--- RoundTrip.cpp
+++ RoundTrip.cpp
@@ -3,51 +3,10 @@
 #define in 0
 #define France__ ;
 
-class DSU {
-	public:
-
-		std::vector<int> v1, v2;
-
-		// number of nodes, better do n + 1
-		DSU(int n)
-		{
-			v1.resize(n);
-			v2.resize(n, 0);
-			for (int i = 0; i < n; ++i)
-				v1[i] = i;
-		}
-
-		int find(int i)
-		{
-			if (v1[i] != i)
-				v1[i] = find(v1[i]);
-			return v1[i];
-		}
-
-
-		void unite(int i, int k)
-		{
-			int a = find(i);
-			int b = find(k);
-			if (a != b)
-			{
-				if (v2[a] > v2[b])
-					v1[b] = a;
-				else if (v2[a] < v2[b])
-					v1[a] = b;
-				else
-				{
-					v1[b] = a;
-					v2[a]++;
-				}
-			}
-		}
-
-		bool is_same_set(int a, int b)
-		{
-			return find(a) == find(b);
-		}
-};
+std::vector<std::vector<int>> g;
+std::vector<int> vis;
+std::vector<int> cycle;
+int start_node = -1;
 
 int main()
 {
@@ -56,49 +15,57 @@ int main()
 	std::cout.tie(0);
 	int n, m;
 	std::cin >> n >> m;
-	std::vector<std::vector<int>> next(n + 1, std::vector<int>());
-	DSU dsu(n + 1);
-	while (m--)
+	g.assign(n + 1, {});
+	vis.assign(n + 1, -1);
+	auto dfs = [&](this auto&& dfs, int node, int parent)
+	{
+		if (vis[node] != -1)
+		{
+			start_node = node;
+			cycle.push_back(node);
+			return 1;
+		}
+
+		vis[node] = 1;
+
+		for (int nxt : g[node])
+		{
+			if (nxt == parent) continue;
+
+			int state = dfs(nxt, node);
+
+			if (state == 1)
+			{
+				cycle.push_back(node);
+				if (node == start_node) return 2;
+				else return 1;
+			}
+			else if (state == 2) return 2;
+		}
+
+		vis[node] = 2;
+		return 0;
+	};
+	for (int i = 0; i < m; i++)
 	{
 		int a, b;
 		std::cin >> a >> b;
-		next[a].push_back(b);
-		next[b].push_back(a);
-		dsu.unite(a, b);
+		g[a].push_back(b);
+		g[b].push_back(a);
 	}
-	std::set<int> set;
+
 	for (int i = 1; i <= n; i++)
-		set.insert(dsu.find(i));
-	std::vector<int> vis(n + 1);
-	std::queue<int> q;
-	for (int x : set)
 	{
-		vis[x] = 1;
-		q.push(x);
-	}
-	while (!q.empty())
-	{
-		int node = q.front();
-		q.pop();
-		for (int x : next[node])
+		if (vis[i] == -1 && dfs(i, -1))
 		{
-			if (!vis[x])
-			{
-				q.push(x);
-				vis[x] = vis[node] == 1 ? 2 : 1;
-			}
-			if (vis[x] == vis[node])
-			{
-				std::cout << "IMPOSSIBLE" << std::endl;
-				return 0;
-			}
+			std::reverse(cycle.begin(), cycle.end());
+			std::cout << cycle.size() << std::endl;
+			for (int x : cycle)
+				std::cout << x << " ";
+			std::cout << std::endl;
+			return 0;
 		}
 	}
-	for (int i = 1; i <= n; i++)
-	{
-		if (vis[i] == 0) vis[i] = 1;
-	}
-	std::copy(vis.begin() + 1, vis.end(), std::ostream_iterator<int>(std::cout, " "));
-	std::cout << std::endl;
+	std::cout << "IMPOSSIBLE" << std::endl;
 	__Made in France__
 }
